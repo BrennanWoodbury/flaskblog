e1cb2b4a9648ad743599fd19e3aed706946561ee
BtheDUB <bthedub@gmail.com>
2020-03-17 22:12:47 -0600

    adding forgot-password functionality /// This is before I have Email server to send out token ///

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -18,16 +18,12 @@ class RegistrationForm(FlaskForm):
     submit = SubmitField("Sign Up")
 
     def validate_username(self, username):
-
         user = User.query.filter_by(username=username.data).first()
-
         if user:
             raise ValidationError("That username is taken. Please choose another.")
 
     def validate_email(self, email):
-
         user = User.query.filter_by(email=email.data).first()
-
         if user:
             raise ValidationError("That email is taken. Please choose another.")
 
@@ -63,3 +59,19 @@ class PostForm(FlaskForm):
     title = StringField('Title', validators=[DataRequired()])
     content = TextAreaField('Content', validators=[DataRequired()])
     submit = SubmitField('Post')
+
+class RequestResetForm(FlaskForm):
+    email = StringField("Email", validators=[DataRequired(), Email()])
+    submit = SubmitField("Update")
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user is None:
+            raise ValidationError("There is no account with that email. You must register first.")
+
+class ResetPasswordForm(FlaskForm):
+    password = PasswordField("Password", validators=[DataRequired()])
+    confirm_password = PasswordField(
+        "Confirm Password", validators=[DataRequired(), EqualTo("password")]
+    )
+    submit = SubmitField("Reset Password")
